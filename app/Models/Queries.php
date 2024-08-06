772056c1ccf76e9b92a3eaa4ffe63f0672da067a
pasindu-dil <dilshanpasindu357@gmail.com>
2024-08-06 10:45:48 +0530

    Update table name in  alterTableCdrCall query.

--- app/Models/Queries.php
+++ app/Models/Queries.php
@@ -101,7 +101,7 @@ class Queries
             "ALTER TABLE `cr_cc_asr_mou` ADD `roaming` TINYINT NULL DEFAULT '0' AFTER `traffic_type`",
             "ALTER TABLE `total_mou_on_wk_day` ADD `roaming` TINYINT NULL DEFAULT '0' AFTER `traffic_type`",
 
-            "ALTER TABLE `ct_mou_top_dest` DROP INDEX `ct_mou_top_dest_date_country_id_traffic_type_unique`, ADD UNIQUE `ct_mou_top_dest_date_country_id_traffic_type_roaming_unique` (`date`, `country_id`, `traffic_type`, `roaming`)",
+            "ALTER TABLE `ct_mou_top_dest` DROP INDEX `ct_mou_top_dest_date_country_id_unique`, ADD UNIQUE `ct_mou_top_dest_date_country_id_traffic_type_roaming_unique` (`date`, `country_id`, `traffic_type`, `roaming`)",
             "ALTER TABLE `ct_mou_variance` DROP INDEX `ct_mou_variance_date_country_id_traffic_type_unique`, ADD UNIQUE `ct_mou_variance_date_country_id_traffic_type_roaming_unique` (`date`,`country_id`,`traffic_type`,`roaming`)",
             "ALTER TABLE `nw_cc_top_dest` DROP INDEX `date_country_id_network_id_plmn_id_traffic_type_unique`, ADD UNIQUE `date_country_id_network_id_plmn_id_traffic_type_roaming_unique` (`date`,`country_id`,`network_id`,`plmn_id`,`traffic_type`,`roaming`)",
             "ALTER TABLE `cr_acd_mou` DROP INDEX `unique_report_date_carrier_id_traffic_type`, ADD UNIQUE `unique_report_date_carrier_id_traffic_type_roaming_type` (`report_date`,`carrier_id`,`traffic_type`,`roaming`)",
@@ -159,7 +159,7 @@ class Queries
 
     public function alterTableCdrCall(): void
     {
-        $cdrTableName = "cdr_call_" . date('Ymd');
+        $cdrTableName = "cdr_call";
         $query = "ALTER TABLE " . $cdrTableName . " ADD `roaming` TINYINT NULL DEFAULT '0' AFTER `network_id`";
         $statement = $this->conn->prepare($query);
         if (!($statement->execute())) {
