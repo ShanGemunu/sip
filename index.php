b558c1d0bbbc6d70c204b7f39840b6042cc5a531
Gemunu Abeysundara <aggabeysundara@gmail.com>
2024-10-08 17:00:42 +0530

    commit at 08/10

--- index.php
+++ index.php
@@ -21,84 +21,85 @@ DbConnection::getDbConnectionInstance([
     'DB_NAME' => $_ENV['DB_NAME'],
     'PORT' => $_ENV['PORT']
 ]);
-$queries = new Queries();
+$queries = new Queries($_ENV['DB_NAME']);
 $currentDate = DateTimeClass::getCurrentDateTime("Y-m-d");
-if($argc === 2){
-    if(preg_match('/^\d{4}-\d{2}-\d{2}$/', $argv[1])){
+if ($argc === 2) {
+    if (preg_match('/^\d{4}-\d{2}-\d{2}$/', $argv[1])) {
         $currentDate = $argv[1];
     }
 }
 
 $tables = [
-    'activity_log' => ["created_at", "updated_at"],
-    'alarms_carrier_history' => ["date_time"],
-    'alarms_country_history' => ["date_time"],
-    'alarms_network_history' => ["date_time"],
-    'carrier_wise_traffic1' => ["date"],
-    'cdr_dialogs' => ["time", "ringing", "answered", "end"],
-    'cr_acd_mou' => ["report_date"],
-    'cr_cc_asr_mou' => ["report_date"],
-    'ct_mou_top_dest' => ["date"],
-    'ct_mou_variance' => ["date"],
-    'flash_call_labels' => ["update_time"],
-    'idd_carrier_level_quality_hourly_v3' => ["date_time"],
-    'idd_hourly_carrier_level_quality_score' => ["date_time"],
-    'idd_hourly_country_level_quality_score' => ["date_time"],
-    'idd_hourly_network_level_quality_score' => ["date_time"],
-    'idd_hourly_stats' => ["date_"],
-    'idd_hourly_stats_v1' => ["date_"],
-    'idd_hourly_trend_outlier_carrier_level' => ["date_time"],
-    'idd_hourly_trend_outlier_country_level' => ["date_time"],
-    'idd_hourly_trend_outlier_network_level' => ["date_time"],
-    'idd_outlier_alarm_history' => ["created_at"],
-    'idd_outlier_hourly' => ["running_time"],
-    'idd_outlier_hourly_tmp' => ["running_time"],
-    'idd_outlier_ten_min' => ["running_time"],
-    'j_cdr_sip_20211001' => ["time"],
-    'j_idd_hourly_stats' => ["date_"],
-    'j_network_data' => ["created_at", "updated_at", "deleted_at"],
-    'j_networks' => ["created_at", "updated_at", "deleted_at"],
-    'nw_cc_top_dest' => ["date"],
-    'report_daily_72_78_outgoing_traffic' => ["date"],
-    'report_daily_country_carrier_wise_traffic' => ["date"],
-    'report_daily_country_carrier_wise_traffic1' => ["date"],
-    'report_daily_country_carrier_wise_traffic2' => ["date"],
-    'report_daily_country_carrier_wise_traffic_quality' => ["date"],
-    'report_hourly_country_carrier_wise_traffic' => ["hour"],
-    'report_hourly_country_carrier_wise_traffic_old' => ["hour"],
-    'report_hourly_country_carrier_wise_traffic_test' => ["hour"],
-    'report_hourly_country_carrier_wise_traffic_test_2' => ["hour"],
-    'report_hourly_country_network_carrier_wise_traffic' => ["hour"],
-    'system_parameters' => ["created_at", "updated_at", "deleted_at"],
-    'temp1_cdr_call_20210217' => ["invite_time", "ringing_time", "answered_time", "ack_time", "bye_time", "cancel_time"],
-    'temp1_cdr_sip_20210217' => ["time"],
-    'temp_cdr_call_20210216' => ["invite_time", "ringing_time", "answered_time", "ack_time", "bye_time", "cancel_time"],
-    'temp_cdr_call_20210217' => ["invite_time", "ringing_time", "answered_time", "ack_time", "bye_time", "cancel_time"],
-    'temp_cdr_sip_20210217' => ["time"],
-    'total_mou_on_wk_day' => ["report_date"]
+    // 'activity_log' => ["created_at", "updated_at"],
+    // 'alarms_carrier_history' => ["date_time"],
+    // 'alarms_country_history' => ["date_time"],
+    // 'alarms_network_history' => ["date_time"],
+    // 'carrier_wise_traffic1' => ["date"],
+    // 'cdr_dialogs' => ["time", "ringing", "answered", "end"],
+    // 'cr_acd_mou' => ["report_date"],
+    // 'cr_cc_asr_mou' => ["report_date"],
+    // 'ct_mou_top_dest' => ["date"],
+    // 'ct_mou_variance' => ["date"],
+    // 'flash_call_labels' => ["update_time"],
+    // 'idd_carrier_level_quality_hourly_v3' => ["date_time"],
+    // 'idd_hourly_carrier_level_quality_score' => ["date_time"],
+    // 'idd_hourly_country_level_quality_score' => ["date_time"],
+    // 'idd_hourly_network_level_quality_score' => ["date_time"],
+    // 'idd_hourly_stats' => ["date_"],
+    // 'idd_hourly_stats_v1' => ["date_"],
+    // 'idd_hourly_trend_outlier_carrier_level' => ["date_time"],
+    // 'idd_hourly_trend_outlier_country_level' => ["date_time"],
+    // 'idd_hourly_trend_outlier_network_level' => ["date_time"],
+    // 'idd_outlier_alarm_history' => ["created_at"],
+    // 'idd_outlier_hourly' => ["running_time"],
+    // 'idd_outlier_hourly_tmp' => ["running_time"],
+    // 'idd_outlier_ten_min' => ["running_time"],
+    // 'j_cdr_sip_20211001' => ["time"],
+    // 'j_idd_hourly_stats' => ["date_"],
+    // 'j_network_data' => ["created_at", "updated_at", "deleted_at"],
+    // 'j_networks' => ["created_at", "updated_at", "deleted_at"],
+
+    // 'nw_cc_top_dest' => ["date"],
+
+    // 'report_daily_72_78_outgoing_traffic' => ["date"],
+    // 'report_daily_country_carrier_wise_traffic' => ["date"],
+    // 'report_daily_country_carrier_wise_traffic1' => ["date"],
+    // 'report_daily_country_carrier_wise_traffic2' => ["date"],
+    // 'report_daily_country_carrier_wise_traffic_quality' => ["date"],
+    // 'report_hourly_country_carrier_wise_traffic' => ["hour"],
+    // 'report_hourly_country_carrier_wise_traffic_old' => ["hour"],
+    // 'report_hourly_country_carrier_wise_traffic_test' => ["hour"],
+    // 'report_hourly_country_carrier_wise_traffic_test_2' => ["hour"],
+    // 'report_hourly_country_network_carrier_wise_traffic' => ["hour"],
+    // 'system_parameters' => ["created_at", "updated_at", "deleted_at"],
+    // 'temp1_cdr_call_20210217' => ["invite_time", "ringing_time", "answered_time", "ack_time", "bye_time", "cancel_time"],
+    // 'temp1_cdr_sip_20210217' => ["time"],
+    // 'temp_cdr_call_20210216' => ["invite_time", "ringing_time", "answered_time", "ack_time", "bye_time", "cancel_time"],
+    // 'temp_cdr_call_20210217' => ["invite_time", "ringing_time", "answered_time", "ack_time", "bye_time", "cancel_time"],
+    // 'temp_cdr_sip_20210217' => ["time"],
+    // 'total_mou_on_wk_day' => ["report_date"]
 ];
 
 try {
     // latest cdr_call table
-    $latestCdrCallTable = $queries->getLatestTableNames($_ENV['DB_NAME'], "cdr_call_", "_bkp", ['start' => 10, 'length' => 8], 1);
+    $latestCdrCallTable = $queries->getLatestTableNames("cdr_call_", "_bkp", ['start' => 10, 'length' => 8], 1);
     // latest cdr_sip table
-    $latestCdrSipTable = $queries->getLatestTableNames($_ENV['DB_NAME'], "cdr_sip_", "_bkp", ['start' => 9, 'length' => 8], 1);
+    $latestCdrSipTable = $queries->getLatestTableNames("cdr_sip_", "_bkp", ['start' => 9, 'length' => 8], 1);
 
     if ($latestCdrCallTable) {
         $dateLatestCdrCallTable = substr($latestCdrCallTable[0]['table_name'], 9);
         $dateDiffData = Logic::findDateDifferenceData($currentDate, $dateLatestCdrCallTable);
 
         if ($dateDiffData['diff'] > 0 && $dateDiffData['diff'] < 7 && $dateDiffData['direction'] === 1) {
-            $latestCdrCallTables = $queries->getLatestTableNames($_ENV['DB_NAME'], "cdr_call_", "_bkp", ['start' => 10, 'length' => 8], $dateDiffData['diff']);
+            $latestCdrCallTables = $queries->getLatestTableNames("cdr_call_", "_bkp", ['start' => 10, 'length' => 8], $dateDiffData['diff']);
         } elseif ($dateDiffData['diff'] > 0 && $dateDiffData['diff'] >= 7 && $dateDiffData['direction'] === 1) {
-            $latestCdrCallTables = $queries->getLatestTableNames($_ENV['DB_NAME'], "cdr_call_", "_bkp", ['start' => 10, 'length' => 8], 7);
+            $latestCdrCallTables = $queries->getLatestTableNames("cdr_call_", "_bkp", ['start' => 10, 'length' => 8], 7);
         }
         if (isset($latestCdrCallTables)) {
             $date = new DateTime($currentDate);
             foreach ($latestCdrCallTables as $table) {
                 $dateFor = $date->format('Ymd');
-                $queries->copyTableStructure($table['table_name'], "cdr_call_{$dateFor}");
-                $queries->copyTableData($table['table_name'], "cdr_call_{$dateFor}");
+                $queries->copyTable($table['table_name'], "cdr_call_{$dateFor}");
                 $tables["cdr_call_{$dateFor}"] = ["invite_time", "ringing_time", "answered_time", "ack_time", "bye_time", "cancel_time"];
                 $date->modify('-1 day');
             }
@@ -109,16 +110,15 @@ try {
         $dateDiffData = Logic::findDateDifferenceData($currentDate, $dateLatestCdrSipTable);
 
         if ($dateDiffData['diff'] > 0 && $dateDiffData['diff'] < 7 && $dateDiffData['direction'] === 1) {
-            $latestCdrSipTables = $queries->getLatestTableNames($_ENV['DB_NAME'], "cdr_sip_", "_bkp", ['start' => 9, 'length' => 8], $dateDiffData['diff']);
+            $latestCdrSipTables = $queries->getLatestTableNames("cdr_sip_", "_bkp", ['start' => 9, 'length' => 8], $dateDiffData['diff']);
         } elseif ($dateDiffData['diff'] > 0 && $dateDiffData['diff'] >= 7 && $dateDiffData['direction'] === 1) {
-            $latestCdrSipTables = $queries->getLatestTableNames($_ENV['DB_NAME'], "cdr_sip_", "_bkp", ['start' => 9, 'length' => 8], 7);
+            $latestCdrSipTables = $queries->getLatestTableNames("cdr_sip_", "_bkp", ['start' => 9, 'length' => 8], 7);
         }
         if (isset($latestCdrSipTables)) {
             $date = new DateTime($currentDate);
             foreach ($latestCdrSipTables as $table) {
                 $dateFor = $date->format('Ymd');
-                $queries->copyTableStructure($table['table_name'], "cdr_sip_{$dateFor}");
-                $queries->copyTableData($table['table_name'], "cdr_sip_{$dateFor}");
+                $queries->copyTable($table['table_name'], "cdr_sip_{$dateFor}");
                 $tables["cdr_sip_{$dateFor}"] = ["time"];
                 $date->modify('-1 day');
             }
@@ -141,7 +141,7 @@ try {
             }
             if ($dateDiffData['direction'] === 1) {
                 $columnsWithData[$column] = ['modifier' => "add", 'dateDiff' => $dateDiffData['diff']];
-            } 
+            }
             Log::logInfo("no controller, but in index file", "no function", "there is difference between recent date from column and provided date", "success", "table - $tableName; column - $column; diff - {$dateDiffData['diff']}; direction - {$dateDiffData['direction']}");
 
         }
@@ -153,11 +153,11 @@ try {
     }
 
 } catch (PrepareQueryFailedException $exception) {
-    Log::logError("no controller, but in index file", "no function", "Exception raised...", "failed", $exception->getMessage());
+    Log::logError("no controller, in index file", "no function", "Exception raised...", "failed", $exception->getMessage());
 } catch (QueryExecuteFailedException $exception) {
-    Log::logError("no controller, but in index file", "no function", "Exception raised...", "failed", $exception->getMessage());
+    Log::logError("no controller, in index file", "no function", "Exception raised...", "failed", $exception->getMessage());
 } catch (Exception $exception) {
-    Log::logError("no controller, but in index file", "no function", "Exception raised...", "failed", $exception->getMessage());
+    Log::logError("no controller, in index file", "no function", "Exception raised...", "failed", $exception->getMessage());
 }
 
 
--- models/Queries.php
+++ models/Queries.php
@@ -10,6 +10,11 @@ use app\exceptions\QueryExecuteFailedException;
 
 class Queries
 {
+    private static $dbName;
+
+    function __construct($db_name){
+        self::$dbName = $db_name;
+    }
 
     /** 
      *    prepare query
@@ -51,6 +56,52 @@ class Queries
     }
 
     /** 
+     *    get unique coulmn names of a table
+     *    @param string $table 
+     *    @param array $columnsWithData 
+     *    @return array|bool
+     */
+    public function getUniqueCoulmnNames(string $tableName) : array|bool
+    {
+        $dbName = self::$dbName;
+
+        $query = "
+            SELECT COLUMN_NAME AS column_name
+            FROM information_schema.STATISTICS
+            WHERE TABLE_SCHEMA = '$dbName'  
+            AND TABLE_NAME = '$tableName'
+            AND NON_UNIQUE = 0;
+        ";
+        Log::logInfo("Queries", "getUniqueCoulmnNames", "trying to get unique column names of a table", "success", "db name - $dbName; table - $tableName");
+
+        $statement = $this->prepareQuery($query);
+
+        if ($statement === false)
+            throw new PrepareQueryFailedException("failed query - $query", Queries::class, "getUniqueCoulmnNames");
+
+        if ($statement->execute() === false) {
+            throw new QueryExecuteFailedException("failed query - $query", Queries::class, "getUniqueCoulmnNames");
+        }
+        $result = $statement->get_result();
+
+        // $resultArray[0]['column_name]
+        $resultArray = $result->fetch_all(MYSQLI_ASSOC);
+
+        if (count($resultArray) === 0) {
+            Log::logInfo("Queries", "getUniqueCoulmnNames", "get unique column names of a table", "success", "no unique columns");
+
+            return false;
+        }
+        $logData = "";
+        foreach($resultArray as $columnNames){
+            $logData .= $columnNames['column_name']."|";
+        }
+        Log::logInfo("Queries", "getUniqueCoulmnNames", "get unique column names of a table", "success", "unique columns - $logData");
+
+        return $resultArray;
+    }
+
+    /** 
      *    update dates of tables
      *    @param string $table 
      *    @param array $columnsWithData 
@@ -59,8 +110,16 @@ class Queries
     public function updateDates(string $table, array $columnsWithData): int
     {
         $setData = "";
+        $uniqueColumnNames = $this->getUniqueCoulmnNames($table);
+        $orderBy = "";
 
         foreach ($columnsWithData as $columnName => $data) {
+            if($uniqueColumnNames){
+                if(in_array($columnName, $uniqueColumnNames)){
+                    $orderBy = "ORDER BY $columnName";
+                }
+            }
+            
             $modifier = ($data['modifier'] === "add") ? "DATE_ADD" : "DATE_SUB";
             $dateDiff = $data['dateDiff'];
             $setData .= "$columnName = IF($columnName IS NOT NULL ,$modifier($columnName, INTERVAL $dateDiff DAY),$columnName),";
@@ -70,8 +129,26 @@ class Queries
 
         $query = "
             UPDATE $table SET 
-                $setData 
+                $setData $orderBy 
         ";
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
 
         $statement = $this->prepareQuery($query);
 
@@ -86,12 +163,13 @@ class Queries
         return $statement->affected_rows;
     }
 
-    public function getLatestTableNames(string $dbName, string $tablePrefix, string $tableRemovePart, array $numericPart, int $limit): array|bool
+    public function getLatestTableNames(string $tablePrefix, string $tableRemovePart, array $numericPart, int $limit): array|bool
     {
         $numericPartStart = $numericPart['start'];
         $numericPartLength = $numericPart['length'];
+        $dbName = self::$dbName;
         $query = "
-            SELECT table_name
+            SELECT table_name AS table_name
             FROM information_schema.tables
             WHERE table_schema = '$dbName'
             AND table_name LIKE '{$tablePrefix}%' 
@@ -121,39 +199,40 @@ class Queries
         return $resultArray;
     }
 
-    public function copyTableStructure(string $originalTable,string $newTable)
+    public function copyTable(string $originalTable, string $newTable)
     {
+        //   CREATE TABLE $newTable LIKE $originalTable
         $query = "
-            CREATE TABLE $newTable LIKE $originalTable
+            CREATE TABLE $newTable SELECT * FROM $originalTable
         ";
 
         $statement = $this->prepareQuery($query);
 
         if ($statement === false)
-            throw new PrepareQueryFailedException("failed query - $query", Queries::class, "copyTableStructure");
+            throw new PrepareQueryFailedException("failed query - $query", Queries::class, "copyTable");
 
         if ($statement->execute() === false) {
-            throw new QueryExecuteFailedException("failed query - $query", Queries::class, "copyTableStructure");
+            throw new QueryExecuteFailedException("failed query - $query", Queries::class, "copyTable");
         }
-        Log::logInfo("Queries", "copyTableStructure", "copy table structure into new table", "success", "data => original table - $originalTable; new table - $newTable");
+        Log::logInfo("Queries", "copyTable", "copy table structure and data into new table", "success", "data => original table - $originalTable; new table - $newTable");
     }
 
-    public function copyTableData(string $originalTable,string $newTable)
-    {
-        $query = "
-            INSERT INTO $newTable SELECT * FROM $originalTable
-        ";
+    // public function copyTableData(string $originalTable, string $newTable)
+    // {
+    //     $query = "
+    //         INSERT INTO $newTable SELECT * FROM $originalTable
+    //     ";
 
-        $statement = $this->prepareQuery($query);
+    //     $statement = $this->prepareQuery($query);
 
-        if ($statement === false)
-            throw new PrepareQueryFailedException("failed query - $query", Queries::class, "copyTableData");
+    //     if ($statement === false)
+    //         throw new PrepareQueryFailedException("failed query - $query", Queries::class, "copyTableData");
 
-        if ($statement->execute() === false) {
-            throw new QueryExecuteFailedException("failed query - $query", Queries::class, "copyTableData");
-        }
-        Log::logInfo("Queries", "copyTableData", "copy table data into new table", "success", "data => data => original table - $originalTable; new table - $newTable");
-    }
+    //     if ($statement->execute() === false) {
+    //         throw new QueryExecuteFailedException("failed query - $query", Queries::class, "copyTableData");
+    //     }
+    //     Log::logInfo("Queries", "copyTableData", "copy table data into new table", "success", "data => data => original table - $originalTable; new table - $newTable");
+    // }
 }
 
 
