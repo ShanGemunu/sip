f6318c11b195cb140349708b178906aa1c74a6d9
Gemunu Abeysundara <aggabeysundara@gmail.com>
2024-10-22 13:16:16 +0530

    commit at 22/10

--- index.php
+++ index.php
@@ -30,16 +30,16 @@ if ($argc === 2) {
 }
 
 $tables = [
-    'activity_log' => [["created_at", "updated_at"],"id"],
-    'alarms_carrier_history' => [["date_time"],"id"],
-    'alarms_country_history' => [["date_time"],"id"],
-    'alarms_network_history' => [["date_time"],"id"],
-    'cdr_dialogs' => [["time", "ringing", "answered", "end"],"id"],
+    'activity_log' => [["created_at", "updated_at"], "id"],
+    'alarms_carrier_history' => [["date_time"], "id"],
+    'alarms_country_history' => [["date_time"], "id"],
+    'alarms_network_history' => [["date_time"], "id"],
+    'cdr_dialogs' => [["time", "ringing", "answered", "end"], "id"],
     'cr_acd_mou' => [["report_date"], "id"],
-    'cr_cc_asr_mou' => [["report_date"],"id"],
-    'ct_mou_top_dest' => [["date"],"id"],
-    'ct_mou_variance' => [["date"],"id"],
-    'flash_call_labels' => [["update_time"],"cli"],
+    'cr_cc_asr_mou' => [["report_date"], "id"],
+    'ct_mou_top_dest' => [["date"], "id"],
+    'ct_mou_variance' => [["date"], "id"],
+    'flash_call_labels' => [["update_time"], "cli"],
     'idd_hourly_carrier_level_quality_score' => [["date_time"]],
     'idd_hourly_country_level_quality_score' => [["date_time"]],
     'idd_hourly_network_level_quality_score' => [["date_time"]],
@@ -47,17 +47,17 @@ $tables = [
     'idd_hourly_trend_outlier_carrier_level' => [["date_time"]],
     'idd_hourly_trend_outlier_country_level' => [["date_time"]],
     'idd_hourly_trend_outlier_network_level' => [["date_time"]],
-    'idd_outlier_alarm_history' => [["created_at"],"id"],
+    'idd_outlier_alarm_history' => [["created_at"], "id"],
     'idd_outlier_hourly' => [["running_time"]],
     'idd_outlier_ten_min' => [["running_time"]],
-    'nw_cc_top_dest' => [["date"],"id"],
-    'report_daily_72_78_outgoing_traffic' => [["date"],"date"],
+    'nw_cc_top_dest' => [["date"], "id"],
+    'report_daily_72_78_outgoing_traffic' => [["date"], "date"],
     'report_daily_country_carrier_wise_traffic' => [["date"]],
-    'report_daily_country_carrier_wise_traffic_quality' => [["date"],"id"],
-    'report_hourly_country_carrier_wise_traffic' => [["hour"],"id"],
-    'report_hourly_country_carrier_wise_traffic_test' => [["hour"],"id"],
-    'report_hourly_country_network_carrier_wise_traffic' =>[["hour"],"id"],
-    'total_mou_on_wk_day' => [["report_date"],"id"]
+    'report_daily_country_carrier_wise_traffic_quality' => [["date"], "id"],
+    'report_hourly_country_carrier_wise_traffic' => [["hour"], "id"],
+    'report_hourly_country_carrier_wise_traffic_test' => [["hour"], "id"],
+    'report_hourly_country_network_carrier_wise_traffic' => [["hour"], "id"],
+    'total_mou_on_wk_day' => [["report_date"], "id"]
 ];
 
 try {
@@ -66,9 +66,14 @@ try {
     // latest cdr_sip table
     $latestCdrSipTable = $queries->getLatestTableNames("cdr_sip_", "_bkp", ['start' => 9, 'length' => 8], 1);
 
+    $dateDiffGolbal;
+
     if ($latestCdrCallTable) {
         $dateLatestCdrCallTable = substr($latestCdrCallTable[0]['table_name'], 9);
         $dateDiffData = Logic::findDateDifferenceData($currentDate, $dateLatestCdrCallTable);
+
+        $dateDiffGolbal = $dateDiffData;
+        Log::logInfo("no controller, but in index file", "no function", "set value to dateDiffGloabal variable", "success", "date difference - {$dateDiffGolbal['diff']} ; direction - {$dateDiffGolbal['direction']}");
 
         if ($dateDiffData['diff'] > 0 && $dateDiffData['diff'] < $_ENV['TABLE_LIMIT'] && $dateDiffData['direction'] === 1) {
             $latestCdrCallTables = $queries->getLatestTableNames("cdr_call_", "_bkp", ['start' => 10, 'length' => 8], limit: $dateDiffData['diff']);
@@ -106,35 +111,36 @@ try {
             }
         }
     }
-
+    
     foreach ($tables as $tableName => $values) {
         $columnsWithData = [];
         $columns = $values[0];
         foreach ($columns as $column) {
-            $recentDateArray = $queries->getRecentDate($tableName, $column);
-            $recentDate = $recentDateArray[0]['latest_order_date'];
-            if (!$recentDate) {
-                Log::logInfo("no controller, but in index file", "no function", "column has no values", "success", "table - $tableName; column - $column");
-                continue;
-            }
-            $recentDate = substr_replace($recentDate, '', 10);
-            
-            if(substr($tableName, 0, 8) === "cdr_sip_" || substr($tableName, 0, 9) === "cdr_call_"){
-                if(substr($tableName, 0, 8) === "cdr_sip_"){
-                    $dateToAdd = substr($tableName, 8, 4)."-".substr($tableName, 12, 2)."-".substr($tableName, 14, 2);
+            // for cdr_call and cdr_sip tables
+            if (substr($tableName, 0, 8) === "cdr_sip_" || substr($tableName, 0, 9) === "cdr_call_") {
+                $recentDateArray = $queries->getRecentDate($tableName, $column);
+                $recentDate = $recentDateArray[0]['latest_order_date'];
+                if (!$recentDate) {
+                    Log::logInfo("no controller, but in index file", "no function", "column has no values in cdr_sip or cdr_call", "success", "table - $tableName; column - $column");
+                    continue;
+                }
+                $recentDate = substr_replace($recentDate, '', 10);
+
+                if (substr($tableName, 0, 8) === "cdr_sip_") {
+                    $dateToAdd = substr($tableName, 8, 4) . "-" . substr($tableName, 12, 2) . "-" . substr($tableName, 14, 2);
                     $dateDiffData = Logic::findDateDifferenceData($dateToAdd, $recentDate);
-                }else{
-                    $dateToAdd = substr($tableName, 9, 4)."-".substr($tableName, 13, 2)."-".substr($tableName, 15, 2);
+                } else {
+                    $dateToAdd = substr($tableName, 9, 4) . "-" . substr($tableName, 13, 2) . "-" . substr($tableName, 15, 2);
                     $dateDiffData = Logic::findDateDifferenceData($dateToAdd, $recentDate);
                 }
-                
-            }else{
-                $dateDiffData = Logic::findDateDifferenceData($currentDate, $recentDate);
+
+            } else {  // for other tables 
+                    $dateDiffData = $dateDiffGolbal;
             }
-            
+
             if ($dateDiffData['diff'] === 0) {
                 Log::logInfo("no controller, but in index file", "no function", "no difference between recent date from column and provided date", "success", "table - $tableName; column - $column");
-                continue;
+                return;
             }
             if ($dateDiffData['direction'] === 1) {
                 $columnsWithData[$column] = ['modifier' => "add", 'dateDiff' => $dateDiffData['diff']];
@@ -145,7 +151,7 @@ try {
         if (count($columnsWithData) === 0) {
             continue;
         }
-
+        
         isset($values[1]) ? $queries->updateDates($tableName, $columnsWithData, $values[1]) :
             $queries->updateDates($tableName, $columnsWithData);
 
